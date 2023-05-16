8738c405feded386e38ed9affebde6f9ebac2f70
IsaevMilan <isaev.milan@gmail.com>
2023-05-16 16:55:12 +0300

    commit 5.1 final Домашка спринт 10.

--- app/src/main/java/com/example/myplaylistmaker/MediaActivity.kt
+++ app/src/main/java/com/example/myplaylistmaker/MediaActivity.kt
@@ -1,21 +1,26 @@
 package com.example.myplaylistmaker
 
-import android.annotation.SuppressLint
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MediaActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_media)
 
-        val backMbutton = findViewById<ImageView>(R.id.arrowBack2)
+        val backButt = findViewById<ImageView>(R.id.arrowBack2)
 
-        backMbutton.setOnClickListener { onBackPressed()
+        backButt.setOnClickListener {
+            onBackPressed()
         }
-        }
+
+
     }
+}
+
+
+     
---
+++ app/src/main/java/com/example/myplaylistmaker/MediaAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.myplaylistmaker
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MediaAdapter(
+    private val track: List<Track>
+) : RecyclerView.Adapter<MediaViewHolder> () {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MediaViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.track_item, parent, false)
+        return MediaViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MediaViewHolder, position: Int) {
+        holder.bind(track[position])
+    }
+
+    override fun getItemCount(): Int {
+        return track.size
+    }
+
+}
